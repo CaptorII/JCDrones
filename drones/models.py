14f3d568264bb8ad756741dbbf32d8bbb2a030e7
JASON CAPTOR <CAPTOJ@tdm.local>
2023-12-04 13:07:15 +0800

    Refactor - Move functions from views to models

--- drones/models.py
+++ drones/models.py
@@ -1,5 +1,8 @@
 from django.db import models
+from django.http import JsonResponse
+from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
+from djitellopy import TelloSwarm, Tello
 
 
 class User(models.Model):
@@ -22,6 +25,17 @@ class Swarm(models.Model):
     updated_at = models.DateTimeField(default=timezone.now)
     updated_by = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    def swarm_takeoff(self):
+        drones = Drone.objects.filter(swarm_ID=self)
+        drone_ips = [drone.IP_address for drone in drones]
+        print(drone_ips)
+        drone_swarm = TelloSwarm.fromIps(drone_ips)
+        drone_swarm.connect()
+        drone_swarm.takeoff()
+        drone_swarm.rotate_counter_clockwise(180)
+        drone_swarm.land()
+        drone_swarm.end()
+
 
 class Drone(models.Model):
     drone_name = models.CharField(max_length=250)
@@ -31,3 +45,106 @@ class Drone(models.Model):
     updated_at = models.DateTimeField(default=timezone.now)
     swarm_ID = models.ForeignKey(Swarm, on_delete=models.CASCADE)
     updated_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def delete_command(self, request):
+        custom_user = get_current_user(request)
+        django_user = request.user
+        custom_user.delete()
+        django_user.delete()
+
+    def get_battery(self):
+        try:
+            tello = Tello(host=self.IP_address)
+            tello.connect()
+            # Use this for working with mocked_drone
+            state_packet = tello.get_current_state()
+            print(state_packet)
+            for key, value in state_packet.items():
+                if key.strip() == 'bat':
+                    battery_level = int(value.strip())
+                    break
+            # Use this for actual drone
+            # battery_level = tello.get_battery()
+        except Exception as e:
+            print(f"Error retrieving battery level for drone {self.drone_name}: {e}")
+            battery_level = -1
+
+        battery_status = {
+            'name': self.drone_name,
+            'IP': self.IP_address,
+            'MAC': self.MAC_address,
+            'battery': battery_level
+        }
+        return JsonResponse(battery_status)
+
+
+def get_latest_swarm():
+    return Swarm.objects.last()
+
+
+def get_current_user(request):
+    return User.objects.get(username=request.user.username)
+
+
+def get_next_drone_id(swarm):
+    return Drone.objects.filter(swarm_ID=swarm).count() + 1
+
+
+def add_default_drone(user, swarm):
+    new_drone = Drone.objects.create(
+        drone_name=f'Drone {get_next_drone_id(swarm)}',
+        IP_address='127.0.0.1',
+        updated_by=user,
+        swarm_ID=swarm)
+
+
+def add_default_swarm(user) -> Swarm:
+    new_swarm = Swarm.objects.create(
+        swarm_name='Swarm1',
+        updated_by=user)
+    return new_swarm
+
+
+def crud_action(request, model, action):
+    from drones.forms import UpdateDroneForm
+    model_class = model_lookup[model][0]
+    form_class = model_lookup[model][1]
+    form = None
+
+    if action == 'create':
+        form = form_class(request.POST)
+        if form.is_valid():
+            new_instance = form.save(commit=False)
+            new_instance.updated_by = get_current_user(request)
+            if model_class == Drone:
+                new_instance.swarm_ID = Swarm.objects.get(pk=request.POST.get('swarm'))
+            new_instance.save()
+    elif action == 'update':
+        instance_id = request.POST.get('instance_id')
+        instance = get_object_or_404(model_class, pk=instance_id)
+        if model_class == Drone:
+            form = UpdateDroneForm(request.POST, instance=instance)
+        else:
+            form = form_class(request.POST, instance=instance)
+        if form.is_valid():
+            updated_instance = form.save(commit=False)
+            updated_instance.updated_by = get_current_user(request)
+            updated_instance.save()
+    elif action == 'delete':
+        instance_id = request.POST.get('instance_id')
+        try:
+            instance = get_object_or_404(model_class, pk=instance_id)
+            instance.delete()
+        except TypeError as e:
+            print(f"Deletion failed: {e}")
+        except ValueError as e:
+            print(f"Deletion failed: {e}")
+    return form
+
+
+from drones.forms import APForm, SwarmForm, DroneForm
+model_lookup = {
+    'ap': [AP, APForm],
+    'swarm': [Swarm, SwarmForm],
+    'drone': [Drone, DroneForm],
+}
--- drones/views.py
+++ drones/views.py
@@ -1,24 +1,23 @@
 from django.contrib.auth import logout
 from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse
 from django.views import View
-from djitellopy import Tello, TelloSwarm
-from .models import Swarm, Drone, User, AP
-from .forms import APForm, SwarmForm, DroneForm, UpdateDroneForm, UserForm
+from .models import *
+from .forms import *
 from .sync_users import sync_users
 
 
 def index(request):
-    sync_users()
+    sync_users()  # call method to add custom users based on django users
     return render(request, "drones/index.html")
 
 
 def update_email(request):
-    user = User.objects.get(username=request.user.username)
-    form = UserForm(instance=user)
+    user = User.objects.get(username=request.user.username)  # get user from request
+    form = UserForm(instance=user)  # initialise form here so it can be passed to return
 
     if request.method == 'POST':
-        form = UserForm(request.POST, instance=user)
+        form = UserForm(request.POST, instance=user)  # create form with details from POST request
         if form.is_valid():
             form.save()
     return render(request, 'drones/update_email.html', {'form': form})
@@ -27,9 +26,7 @@ def update_email(request):
 def delete_user(request):
     if request.method == 'POST':
         custom_user = get_current_user(request)
-        custom_user.delete()
-        user = request.user
-        user.delete()
+        custom_user.delete_command(request)  # pass to model to delete user objects
         logout(request)
     return redirect('index')
 
@@ -47,161 +44,65 @@ def dashboard(request):
 
 def takeoff(request):
     swarm = Swarm.objects.get(pk=request.POST.get('swarm_id'))
-    drones = Drone.objects.filter(swarm_ID=swarm)
-    drone_ips = [drone.IP_address for drone in drones]
-    print(drone_ips)
-    drone_swarm = TelloSwarm.fromIps(drone_ips)
-    drone_swarm.connect()
-    drone_swarm.takeoff()
-    drone_swarm.rotate_counter_clockwise(180)
-    drone_swarm.land()
-    drone_swarm.end()
+    swarm.swarm_takeoff()
     return render(request, "drones/dashboard.html")
 
 
 def get_battery_status(request, drone_id):
     drone = Drone.objects.get(pk=drone_id)
-
-    try:
-        tello = Tello(host=drone.IP_address)
-        tello.connect()
-        # Use this for working with mocked_drone
-        state_packet = tello.get_current_state()
-        print(state_packet)
-        for key, value in state_packet.items():
-            if key.strip() == 'bat':
-                battery_level = int(value.strip())
-                break
-        # Use this for actual drone
-        # battery_level = tello.get_battery()
-    except Exception as e:
-        print(f"Error retrieving battery level for drone {drone.drone_name}: {e}")
-        battery_level = -1
-
-    battery_status = {
-        'name': drone.drone_name,
-        'IP': drone.IP_address,
-        'MAC': drone.MAC_address,
-        'battery': battery_level
-    }
-    return JsonResponse(battery_status)
-
-
-def get_latest_swarm():
-    return Swarm.objects.last()
-
-
-def get_current_user(request):
-    return User.objects.get(username=request.user.username)
-
-
-def get_next_drone_id(swarm):
-    return Drone.objects.filter(swarm_ID=swarm).count() + 1
+    drone.get_battery()
 
 
 def add_swarm(request):
-    if request.method == 'POST':
-        user = get_current_user(request)
-        new_swarm = Swarm.objects.create(
-            swarm_name='Swarm1',
-            updated_by=user)
-        aps = AP.objects.all()
-        ap_id = request.GET.get('ap_id')
-        active_ap = get_object_or_404(AP, id=ap_id) if ap_id else None
-        swarms = Swarm.objects.all()
-        drones = Drone.objects.filter(swarm_ID=new_swarm)
-        context = {'aps': aps, 'active_ap': active_ap, 'swarms': swarms, 'swarm': new_swarm, 'drones': drones}
-        return render(request, 'drones/dashboard.html', context)
-    else:
+    if request.method != 'POST':
         return HttpResponse('Invalid request')
+    user = get_current_user(request)
+    new_swarm = add_default_swarm(user)  # pass to model to create new drone
+    aps = AP.objects.all()
+    ap_id = request.GET.get('ap_id')
+    active_ap = get_object_or_404(AP, id=ap_id) if ap_id else None
+    swarms = Swarm.objects.all()
+    drones = Drone.objects.filter(swarm_ID=new_swarm)
+    context = {'aps': aps, 'active_ap': active_ap, 'swarms': swarms, 'swarm': new_swarm, 'drones': drones}
+    return render(request, 'drones/dashboard.html', context)
 
 
 def add_drone(request):
-    if request.method == 'POST':
-        swarm_id = request.POST.get('swarm_id')
-        swarm = Swarm.objects.get(pk=swarm_id)
-        user = get_current_user(request)
-        new_drone = Drone.objects.create(
-            drone_name=f'Drone {get_next_drone_id(swarm)}',
-            IP_address='127.0.0.1',
-            updated_by=user,
-            swarm_ID=swarm)
-        aps = AP.objects.all()
-        ap_id = request.GET.get('ap_id')
-        active_ap = get_object_or_404(AP, id=ap_id) if ap_id else None
-        swarms = Swarm.objects.all()
-        drones = Drone.objects.filter(swarm_ID=swarm)
-        context = {'aps': aps, 'active_ap': active_ap, 'swarms': swarms, 'swarm': swarm, 'drones': drones}
-        return render(request, "drones/dashboard.html", context)
-    else:
+    if request.method != 'POST':
         return HttpResponse('Invalid request')
+    swarm_id = request.POST.get('swarm_id')
+    swarm = Swarm.objects.get(pk=swarm_id)
+    user = get_current_user(request)
+    add_default_drone(user, swarm)  # pass to model to create new drone
+    aps = AP.objects.all()
+    ap_id = request.GET.get('ap_id')
+    active_ap = get_object_or_404(AP, id=ap_id) if ap_id else None
+    swarms = Swarm.objects.all()
+    drones = Drone.objects.filter(swarm_ID=swarm)
+    context = {'aps': aps, 'active_ap': active_ap, 'swarms': swarms, 'swarm': swarm, 'drones': drones}
+    return render(request, "drones/dashboard.html", context)
 
 
 class CRUDView(View):
     template_name = 'drones/crud/crud_page.html'
 
     def get(self, request, model, *args, **kwargs):
-        instances = None
-        form = None
+        instances = model_lookup[model][0].objects.all()
+        form = model_lookup[model][1]
 
-        if model == 'ap':
-            instances = AP.objects.all()
-            form = APForm()
-        elif model == 'swarm':
-            instances = Swarm.objects.all()
-            form = SwarmForm()
-        elif model == 'drone':
+        if model == 'drone':
             swarm_id = request.GET.get('swarm_id')
             form = DroneForm(swarm_id)
-            instances = Drone.objects.all()
 
         return render(request, self.template_name, {'model': model, 'instances': instances, 'form': form})
 
 
 class CRUDActionView(View):
-    template_name = 'drones/crud/crud_page.html'
-
     def post(self, request, model, action, *args, **kwargs):
-        form = None
-
-        if model == 'ap':
-            form_class = APForm
-            model_class = AP
-        elif model == 'swarm':
-            form_class = SwarmForm
-            model_class = Swarm
-        elif model == 'drone':
-            form_class = DroneForm
-            model_class = Drone
-
-        if action == 'create':
-            form = form_class(request.POST)
-            if form.is_valid():
-                new_instance = form.save(commit=False)
-                new_instance.updated_by = get_current_user(request)
-                if model_class == Drone:
-                    new_instance.swarm_ID = Swarm.objects.get(pk=request.POST.get('swarm'))
-                new_instance.save()
-        elif action == 'update':
-            instance_id = request.POST.get('instance_id')
-            instance = get_object_or_404(model_class, pk=instance_id)
-            if model_class == Drone:
-                form = UpdateDroneForm(request.POST, instance=instance)
-            else:
-                form = form_class(request.POST, instance=instance)
-            if form.is_valid():
-                updated_instance = form.save(commit=False)
-                updated_instance.updated_by = get_current_user(request)
-                updated_instance.save()
-        elif action == 'delete':
-            instance_id = request.POST.get('instance_id')
-            try:
-                instance = get_object_or_404(model_class, pk=instance_id)
-                instance.delete()
-            except Exception as e:
-                print(f"Deletion failed: {e}")
-
+        template_name = 'drones/crud/crud_page.html'
+        form = crud_action(request, model, action)  # take actions within the model
+        model_class = model_lookup[model][0]
         instances = model_class.objects.all()
         selected_instance_id = request.POST.get('instance_id')
-        return render(request, self.template_name,
+        return render(request, template_name,
                       {'model': model, 'instances': instances, 'form': form, 'action': action, 'selected_instance_id': selected_instance_id})
